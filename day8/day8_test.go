0192e466cfbdb0b79adaaf7b61c56e218d639e7a
tomaskul <>
2022-12-08 23:10:28 +0000

    Improving tests for day 8

--- day8/day8_test.go
+++ day8/day8_test.go
@@ -13,26 +13,66 @@ func Test_GetIntRow_MatchesExpected(t *testing.T) {
 	}
 }
 
+type GridTestCase struct {
+	Name          string
+	Input         [][]int
+	Expected      [][]int
+	ExpectedCount int
+}
+
 func Test_GetVisibilityGrid_MatchesExpected(t *testing.T) {
-	input := [][]int{
-		0: {3, 0, 3, 7, 3},
-		1: {2, 5, 5, 1, 2},
-		2: {6, 5, 3, 3, 2},
-		3: {3, 3, 5, 4, 9},
-		4: {3, 5, 3, 9, 0}}
-	expected := [][]int{
-		0: {1, 1, 1, 1, 1},
-		1: {1, 1, 1, 0, 1},
-		2: {1, 1, 0, 1, 1},
-		3: {1, 0, 1, 0, 1},
-		4: {1, 1, 1, 1, 1},
+	testCases := []GridTestCase{
+		{
+			Name: "Example",
+			Input: [][]int{
+				0: {3, 0, 3, 7, 3},
+				1: {2, 5, 5, 1, 2},
+				2: {6, 5, 3, 3, 2},
+				3: {3, 3, 5, 4, 9},
+				4: {3, 5, 3, 9, 0},
+			},
+			Expected: [][]int{
+				0: {1, 1, 1, 1, 1},
+				1: {1, 1, 1, 0, 1},
+				2: {1, 1, 0, 1, 1},
+				3: {1, 0, 1, 0, 1},
+				4: {1, 1, 1, 1, 1},
+			},
+			ExpectedCount: 21,
+		},
+		{
+			Name: "Real data sample",
+			Input: [][]int{
+				0: {2, 0, 1, 2, 2, 1, 2, 2, 2, 3, 1},
+				1: {2, 0, 1, 1, 1, 0, 2, 2, 3, 2, 1},
+				2: {0, 2, 2, 0, 2, 0, 0, 2, 1, 3, 0},
+				3: {2, 2, 0, 1, 0, 1, 3, 0, 0, 1, 0},
+				4: {0, 1, 0, 0, 1, 3, 2, 0, 3, 2, 1},
+				5: {2, 1, 2, 2, 2, 2, 2, 3, 2, 3, 1},
+				6: {0, 0, 0, 2, 1, 0, 3, 3, 0, 2, 1},
+			},
+			Expected: [][]int{
+				0: {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+				1: {1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1},
+				2: {1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1},
+				3: {1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1},
+				4: {1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1},
+				5: {1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1},
+				6: {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+			},
+			ExpectedCount: 50,
+		},
 	}
 
-	visibilityGrid, totalVisible := getVisibilityGrid(input)
-	if !reflect.DeepEqual(expected, visibilityGrid) {
-		t.Errorf("Expected:\n%v\ngot:\n%v", expected, visibilityGrid)
-	}
-	if totalVisible != 21 {
-		t.Errorf("Expected total visible: %d, got: %d", 21, totalVisible)
+	for _, tC := range testCases {
+		t.Run(tC.Name, func(t *testing.T) {
+			visibilityGrid, totalVisible := getVisibilityGrid(tC.Input)
+			if !reflect.DeepEqual(tC.Expected, visibilityGrid) {
+				t.Errorf("Expected:\n%v\ngot:\n%v", tC.Expected, visibilityGrid)
+			}
+			if totalVisible != tC.ExpectedCount {
+				t.Errorf("Expected total visible: %d, got: %d", tC.ExpectedCount, totalVisible)
+			}
+		})
 	}
 }
